c73bc06abadcbad14fb5863094f770f7e6ebf159
陈楠 <netbadge@gmail.com>
2025-09-24 07:22:16 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -36,9 +36,8 @@ export default function AppHeader() {
         ))}
       </nav>
       
-      <p className="text-xs text-muted-foreground mr-4">这是原型演示切换功能用的菜单，不是需求中的一部分</p>
-
       <div className="flex items-center gap-4 md:gap-2 lg:gap-4">
+        <p className="text-xs text-muted-foreground mr-4">这是原型演示切换功能用的菜单，不是需求中的一部分</p>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" size="icon" className="rounded-full">
