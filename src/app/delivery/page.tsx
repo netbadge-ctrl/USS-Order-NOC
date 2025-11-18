7b3d0f2b111fbbbac3011bbd900fcbf25aeda105
陈楠 <netbadge@gmail.com>
2025-11-18 06:24:52 +0000

    查看改配方案中增加两个文字提醒，要求红色字体加粗“如果服务器当前机房库存满足需求，则先改配后搬迁，如果当前机房配件库存不足，则需将配件调配只目标

--- src/app/delivery/page.tsx
+++ src/app/delivery/page.tsx
@@ -1065,6 +1065,10 @@ function DeliveryPage() {
                                 : `以下为检测到的需要进行硬件改配的服务器方案详情。您可以直接修改规格、Model和数量。`}
                             </AlertDialogDescription>
                         </AlertDialogHeader>
+                         <div className="space-y-1 text-xs pt-2">
+                            <p className="text-red-600 font-bold">如果服务器当前机房库存满足需求，则先改配后搬迁，如果当前机房配件库存不足，则需将配件调配只目标机房进行改配。</p>
+                            <p className="text-red-600 font-bold">目标机房配件不足时，无法发起改配工单，请先进行配件调拨</p>
+                        </div>
                         {upgradePlanBatches.length > 0 ? (
                         <Tabs defaultValue={activeBatchTab} onValueChange={setActiveBatchTab} className="w-full pt-2">
                             {!focusedSnForPlan && (
@@ -1224,3 +1228,5 @@ export default DeliveryPage;
     
 
       
+
+    
