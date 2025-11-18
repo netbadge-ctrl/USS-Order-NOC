6341c40b0571d930e9fdb92c147a1e7edcfad0cf
陈楠 <netbadge@gmail.com>
2025-11-18 03:13:19 +0000

    不要把查看改配方案的弹窗的宽度定死，要根据弹窗中的内容，自动调整

--- src/app/delivery/page.tsx
+++ src/app/delivery/page.tsx
@@ -996,7 +996,7 @@ function DeliveryPage() {
                 setIsUpgradePlanDialogOpen(true);
             }
         }}>
-            <AlertDialogContent className="!max-w-7xl">
+            <AlertDialogContent className="max-w-none w-auto">
                  <button
                     onClick={() => setIsUpgradePlanDialogOpen(false)}
                     className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
